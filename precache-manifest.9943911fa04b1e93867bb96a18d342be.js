5ae8d750d1789feaf325c54c05e57d9cf09f0b4d
Anh Bien <contact@anhnbien.com>
2019-11-24 23:28:56 -0600

    Updates

---
+++ precache-manifest.9943911fa04b1e93867bb96a18d342be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5c3d099061c7c39afed20f6c9953f0",
+    "url": "/anhnbien.com/index.html"
+  },
+  {
+    "revision": "ec9b1d038502abbba6df",
+    "url": "/anhnbien.com/static/css/main.3a302845.chunk.css"
+  },
+  {
+    "revision": "6da10389600df4082394",
+    "url": "/anhnbien.com/static/js/2.44ae25d9.chunk.js"
+  },
+  {
+    "revision": "ec9b1d038502abbba6df",
+    "url": "/anhnbien.com/static/js/main.b6310c1e.chunk.js"
+  },
+  {
+    "revision": "6e3a5b6588c1e70945ce",
+    "url": "/anhnbien.com/static/js/runtime-main.7b61b78f.js"
+  },
+  {
+    "revision": "8976bcb515f1aaac5f299d15aa3f5754",
+    "url": "/anhnbien.com/static/media/designer.8976bcb5.svg"
+  },
+  {
+    "revision": "9c273e0e283e52bad2cdc3f558139558",
+    "url": "/anhnbien.com/static/media/face.9c273e0e.mp4"
+  },
+  {
+    "revision": "2ae4fb1a63590f9cf07ee9ebaa711afb",
+    "url": "/anhnbien.com/static/media/frontend.2ae4fb1a.svg"
+  },
+  {
+    "revision": "1a1b7bdb29887d50510784b8ccdadc01",
+    "url": "/anhnbien.com/static/media/hero.1a1b7bdb.svg"
+  },
+  {
+    "revision": "98f4b769eea77af06f48850e22823c61",
+    "url": "/anhnbien.com/static/media/logo.98f4b769.svg"
+  }
+]);
